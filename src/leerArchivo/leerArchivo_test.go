4a0a4a2f9b88690d5f36a8cd75d6438ed5e73302
Maria Camila  Gomez <mariacamilagomez@MacBook-Pro-de-Maria.local>
2017-08-20 20:09:53 -0500

    Test leer archivo no existente

--- src/leerArchivo/leerArchivo_test.go
+++ src/leerArchivo/leerArchivo_test.go
@@ -4,9 +4,16 @@ import (
   "testing")
 
 func TestAbrirArchivo(t_test *testing.T){
-  s_texto := abrirArchivo()
+  s_texto := abrirArchivo("archivo.txt")
 
   if (s_texto == "") {
 		t_test.Fatalf("No fue posible abrir el archivo")
 	}
+}
+
+func TestArchivoNoEncontrado(t_test *testing.T){
+  s_texto := abrirArchivo("archivoNoEncontrado.txt")
+  if (s_texto != ""){
+      t_test.Fatalf("El archivo no estiste")
+  }
 }
