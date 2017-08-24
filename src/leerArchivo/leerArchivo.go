7705a2fb6c7bb7d9d586947d6982cdb0991057a8
niltonsteveen <nilton.velez@udea.edu.co>
2017-08-23 20:13:12 -1000

    array con cada una de las lineas del archivo

--- src/leerArchivo/leerArchivo.go
+++ src/leerArchivo/leerArchivo.go
@@ -1,15 +1,30 @@
 package testNilton
 
 import (
-    "fmt"
     "io/ioutil"
+    "os"
+	"bufio"
+	"fmt"
 )
 
 func abrirArchivo(s_nombreArchivo string) string{
-  dat, err := ioutil.ReadFile(s_nombreArchivo)
-  if err != nil {
-      return ""
-  }
-  fmt.Print(string(dat))
-  return string(dat)
+    dat, err := ioutil.ReadFile(s_nombreArchivo)
+    if err != nil {
+    	return ""
+    }
+    return string(dat)
+}
+
+func abrirArchivo2(nombre string) []string{
+    file, err := os.Open(nombre)
+	var array []string
+    if err != nil {
+		fmt.Println(err)
+    }
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan() {
+		array = append(array, scanner.Text())
+	}
+	return  array
 }
