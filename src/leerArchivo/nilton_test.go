2b33fa7f9476872c39cd1f505ef4ddb1e1647fe3
niltonsteveen <nilton.velez@udea.edu.co>
2017-08-23 20:22:54 -1000

    test de encontrar metodos indefinido

--- src/leerArchivo/nilton_test.go
+++ src/leerArchivo/nilton_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestEmpty(t *testing.T ){
+func TestVacio(t *testing.T ){
 	expected := "dsfa"
 	actual := validateCode()
 	if actual!=expected {
@@ -12,9 +12,16 @@ func TestEmpty(t *testing.T ){
 	}
 }
 
-func TestRead(t *testing.T){
+func TestLeer(t *testing.T){
 	valor, array:=readFile()
 	if valor!=true && len(array)<=0 {
 		t.Errorf("El archivo no fue correctamente leido o esta vacio")
+	}
+}
+
+func TestEncontrarMetodos(t *testing.T){
+	lineas:=encontrarMetodos()
+	if len(lineas)<=0 {
+		t.Errorf("En el archivo no hay metodos")
 	}
 }
